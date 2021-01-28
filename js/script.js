377b80b74ead5edc056cfd011d89c59b2e1978c0
emg27198 <darkmagician105@gmail.com>
2021-01-28 11:24:21 -0500

    Progress Portal for Capstone

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//modification date
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").textContent = year;
+
+let modified = document.lastModified;
+document.getElementById("modified").textContent = modified;
