1be77f5412c193b532a99e7492da40ee0162fc13
emg27198 <darkmagician105@gmail.com>
2021-02-11 11:51:48 -0500

    changes to website

--- minecraft/js/script.js
+++ minecraft/js/script.js
@@ -1,7 +1,12 @@
+//navigation menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
 //modification date
 let d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").textContent = year;
 
 let modified = document.lastModified;
-document.getElementById("modified").textContent = modified;
+document.getElementById("modified").textContent = "Last Modified: " + modified;
